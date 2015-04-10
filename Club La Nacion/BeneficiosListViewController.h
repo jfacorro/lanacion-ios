a3fcdc18bc8a8e85809c2319a0991cb7171d9fd0
Sebastian Cancinos <sebacancinos@gmail.com>
2015-04-10 20:38:15 -0300

    Settings on the storyboard for the lists created the view classes files

---
+++ Club La Nacion/BeneficiosListViewController.h
@@ -0,0 +1,13 @@
+//
+//  BeneficiosListViewController.h
+//  Club La Nacion
+//
+//  Created by Sebastian Cancinos on 4/10/15.
+//  Copyright (c) 2015 inaka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BeneficiosListViewController : UICollectionViewController
+
+@end
